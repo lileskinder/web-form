5dfd8531fa663e466cf911ebae9af17e9c16f018
lileskinder <33198174+lileskinder@users.noreply.github.com>
2024-05-28 23:22:27 -0400

    web form

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <SingupForm />
+</template>
+
+<script>
+import SingupForm from './components/Singup-form.vue'
+export default {
+  name: 'App',
+  components: {SingupForm}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, Sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+body{
+  margin: 0;
+  background: #eee;
+}
+</style>
